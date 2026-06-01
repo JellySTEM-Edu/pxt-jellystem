0a6f865daed12aaab21ee57e47d64548d99412d2
jellystem <github@jellystem.us>
2026-06-01 09:49:01 -0400

    Create main.ts
    Add initial JellySTEM block namespace

---
+++ main.ts
@@ -0,0 +1,13 @@
+/**
+ * JellySTEM blocks
+ */
+//% weight=100 color=#3CB371 icon="\uf1b2" block="JellySTEM"
+namespace jellystem {
+    /**
+     * Test block for the first MakeCode extension check.
+     */
+    //% block="JellySTEM test block"
+    export function testBlock(): void {
+        basic.showIcon(IconNames.Yes)
+    }
+}
