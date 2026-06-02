36a65c70525aedca3696dbfb43649173965fde22
jellystem-intern <intern@jellystem.us>
2026-06-02 11:52:15 -0400

    set up branch ready for mShield copy

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-/**
- * JellySTEM blocks
- */
+/*
+* This extension library includes core blocks adapted from the SIYEENOVE team.
+* Original Copyright (c) 2024 SIYEENOVA under the MIT License.
+* Rebranded and adapted for JellySTEM Education.
+*/
+
 //% weight=100 color=#3CB371 icon="\uf1b2" block="JellySTEM"
 namespace jellystem {
     /**
@@ -10,4 +13,6 @@ namespace jellystem {
     export function testBlock(): void {
         basic.showIcon(IconNames.Yes)
     }
+
+    
 }
