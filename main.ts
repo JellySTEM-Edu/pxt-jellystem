9c527da7580ed1865dc5408bd80cbd2a3caa39d4
jellystem-intern <intern@jellystem.us>
2026-06-11 14:15:18 -0400

    fix percentage sign bug in acceleration block

--- main.ts
+++ main.ts
@@ -306,7 +306,7 @@ namespace jellystem {
          */
     //% group="Motors"
     //% blockId=jellystem_motor_accelerate
-    //% block="change %motor| to speed %targetSpeed|% over %duration| ms"
+    //% block="change %motor| to speed %targetSpeed| % over %duration| ms"
     //% targetSpeed.min=-100 targetSpeed.max=100
     //% duration.shadow=timePicker
     //% weight=85
